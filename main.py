e0221b199f7de098134c183356590cda12183f4d
Jingna Li <jingna13@gmail.com>
2019-01-14 09:35:25 -0800

    Modify main.py to call from source files

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-import tweepy
 import json
+from src.tweet_mananger import TweetManager
+from src.file_handler import FileHandler
+import src.teams
 
-afc_east = ("patriots", "miamidolphins", "buffalobills", "nyjets")
-afc_west = ("chiefs", "chargers", "broncos", "raiders")
-afc_north = ("ravens", "steelers", "browns", "bengals")
-afc_south = ("houstontexans", "colts","titans", "jaguars")
-afc = afc_east + afc_west + afc_north + afc_south
-
-nfc_east = ("dallascowboys", "eagles", "redskins", "giants")
-nfc_west = ("rams", "seahawks", "49ers", "cardinals")
-nfc_north = ("chicagobears", "vikings", "packers", "lions")
-nfc_south = ("saints", "atlantafalcons", "panthers", "buccaneers")
-nfc = nfc_east + nfc_west + nfc_north + nfc_south
-
-nfl = afc + nfc
-
-#with open('keys.json') as f:
-#    data = json.load(f)
-
-# Setup the authentication
-#auth = tweepy.OAuthHandler(data["CONSUMER_KEY"], data["CONSUMER_SECRET"])
-#auth.set_access_token(data["ACCESS_TOKEN"], data["ACCESS_SECRET"])
-
-# Create an authenticated connection to the Twitter API using Tweepy
-#api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
-
-def do_something():
+def initialize():
     ''' This is a function that prints Hello World to the console
     '''
-#    statuses = api.user_timeline(screen_name='timbull')
-    print(statuses)
-    print("Hello world!")
+    # get keys
+    with open('keys.json') as f:
+      data = json.load(f)
+
+    print("Starting...")
+
+    # inititalize TweetManager
+    t = TweetManager(data)
+
+    field_names = [ 'id', 'name', 'created_at', 'text', 'media_type']
+
+    data = t.get_all_tweets( src.teams.nfl, field_names )
+    f = FileHandler(data, field_names)
+
+    print("Finished")
+
 
 if __name__ == '__main__':
-    # Entry point for command line
-    do_something()
+    # Entry point for command
+    initialize()
